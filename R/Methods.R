7a9242d6e8dfa919011ddef3213b95488fe13c1e
khazum <khazum@gmail.com>
2022-09-01 15:11:07 -0400

    Update Methods.R

--- R/Methods.R
+++ R/Methods.R
@@ -197,7 +197,6 @@ findNDim <- function(n, distPCA, pcaMin, pcaMax){
     else{
         eigs <- distPCA$sdev^2
         prop <- eigs/sum(eigs)
-        n <- length(prop)
         iVal <- n-findInterval(c(pcaMin, pcaMax), prop[n:1])+1
         nDim <- seq(iVal[2], iVal[1], ceiling((iVal[1]-iVal[2])/14))
     }
